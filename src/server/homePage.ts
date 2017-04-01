9ca7b81d33f79764562383d6019920b4e2bcfe90
ErikCupal <erik@cup.al>
2017-04-01 21:11:27 +0200

    refactored

---
+++ src/server/homePage.ts
@@ -0,0 +1,30 @@
+/** When someone enters the server address in browser, he will see this website */
+export const homePage =
+  `
+  <!DOCTYPE html>
+  </html>
+  <html lang="en">
+    <head>
+      <title>The Black Cat - Server</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="https://bootswatch.com/paper/bootstrap.min.css" rel="stylesheet">
+      <style>
+        body {
+          background-color: DarkSlateGray;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-content: center;
+          width: 100vw;
+          height: 100vh;
+          text-align: center;
+        }
+      </style>
+    </head>
+    <body>
+        <h1 style="color: Beige;">The Black Cat</h1>
+        <h2 style="color: BurlyWood;">The server is up and running</h2>
+    </body>
+  </html>
+  `
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,4 @@
+import { homePage } from './homePage'
 import http from 'http'
 import Express from 'express'
 
@@ -6,37 +7,9 @@ const createServer = (): http.Server => {
   const app = Express()
   const server = http.createServer(app)
 
-  // When someone enters the server address in browser,
-  // it shows this basic website
+  // Routes to the root of server address when opened in a browser
   app.get('/', (req, res) => {
-    res.send(`
-      <!DOCTYPE html>
-      </html>
-      <html lang="en">
-        <head>
-          <title>The Black Cat - Server</title>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1">
-          <link href="https://bootswatch.com/paper/bootstrap.min.css" rel="stylesheet">
-          <style>
-            body {
-              background-color: DarkSlateGray;
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-content: center;
-              width: 100vw;
-              height: 100vh;
-              text-align: center;
-            }
-          </style>
-        </head>
-        <body>
-            <h1 style="color: Beige;">The Black Cat</h1>
-            <h2 style="color: BurlyWood;">The server is up and running</h2>
-        </body>
-      </html>
-    `)
+    res.send(homePage)
   })
 
   return server
